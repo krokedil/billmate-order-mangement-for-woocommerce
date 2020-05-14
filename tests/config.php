34b3fe026a2bf0e71d53f46a0469148bbdc450ac
MichaelBengtsson <michael@krokedil.se>
2020-05-14 08:47:49 +0200

    Add PHP Unit framework and github workflow

---
+++ tests/config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin configuration file.
+ *
+ * @author Krokedil
+ * @since 1.0.0
+ */
+return array(
+	'name'    => 'woocommerce-billmate-order-management.php',
+	'version' => '1.0.0',
+);

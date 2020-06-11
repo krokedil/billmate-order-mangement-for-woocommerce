77ae4c9470ad51c364307a7b80500f3f836da4de
Kristian Andersson <32387117+Kristian-Andersson@users.noreply.github.com>
2020-06-11 14:28:17 +0200

    Add easy access function for credit payment and get payment

--- classes/class-bom-api.php
+++ classes/class-bom-api.php
@@ -41,4 +41,30 @@ class BOM_API {
 
 		return $response;
 	}
+
+	/**
+	 * Get Billmate Payment.
+	 *
+	 * @param string $bco_transaction_id The Billmate transaction id.
+	 * @return mixed
+	 */
+	public function request_get_payment( $bco_transaction_id = '' ) {
+		$request  = new BOM_Request_Get_Payment();
+		$response = $request->request( $bco_transaction_id );
+
+		return $response;
+	}
+
+	/**
+	 * Credit Billmate Payment.
+	 *
+	 * @param string $bco_transaction_id The Billmate transaction id.
+	 * @param string $refund_data The refund data.
+	 * @return mixed
+	 */
+	public function request_credit_payment( $bco_transaction_id = '', $refund_data ) {
+		$request  = new BOM_Request_Credit_Payment();
+		$response = $request->request( $bco_transaction_id, $refund_data );
+		return $response;
+	}
 }

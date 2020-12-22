761eb8143d7663ac8265ad180675a391383dcb73
Michael Bengtsson <michael@krokedil.se>
2020-12-22 12:26:07 +0100

    Add gulpfile, package.json and package-lock.json

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+/* globals require */
+var gulp = require('gulp');
+var sort = require('gulp-sort');
+var wpPot = require('gulp-wp-pot');
+
+var translateFiles = '**/*.php';
+
+gulp.task('makePOT', function () {
+	return gulp.src('**/*.php')
+		.pipe(sort())
+		.pipe(wpPot({
+			domain: 'billmate-order-management-for-woocommerce',
+			destFile: 'src/languages/billmate-order-management-for-woocommerce.pot',
+			package: 'billmate-order-management-for-woocommerce',
+			bugReport: 'http://krokedil.se',
+			lastTranslator: 'Krokedil <info@krokedil.se>',
+			team: 'Krokedil <info@krokedil.se>'
+		}))
+		.pipe(gulp.dest('src/languages/billmate-order-management-for-woocommerce.pot'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(translateFiles, ['makePOT']);
+});
